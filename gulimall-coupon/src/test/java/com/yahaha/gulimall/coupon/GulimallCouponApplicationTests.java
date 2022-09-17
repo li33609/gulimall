a5f855a5da4d065f76029f49d8643231a0d79d3e
yahaha <596000428@qq.com>
2022-09-17 10:38:46 +0800

    项目初始结构

---
+++ gulimall-coupon/src/test/java/com/yahaha/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahaha.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-member/src/test/java/com/yahaha/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahaha.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
